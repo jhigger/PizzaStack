3c5258e1ed844059daa2c44a0b1f1ff18de21e71
jhigger <njohnjhigger@gmail.com>
2021-09-06 16:24:34 +0800

    Added scroll to top on route change

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './components/Home';
+import Franchise from './components/Franchise/Franchise';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
 	return (
 		<Router>
 			<Navigation />
-			<Switch>
-				<Route exact path="/">
-					<Home />
-				</Route>
-				<Route path="/Home">
-					<Home />
-				</Route>
-				<Route path="/Menu">
-					<div>Menu</div>
-				</Route>
-				<Route path="/Franchise">
-					<div>Franchise</div>
-				</Route>
-				<Route path="/About">
-					<div>About</div>
-				</Route>
-				<Route path="/Contact">
-					<div>Contact</div>
-				</Route>
-			</Switch>
+			<ScrollToTop>
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/Home">
+						<Home />
+					</Route>
+					<Route path="/Menu">
+						<div>Menu</div>
+					</Route>
+					<Route path="/Franchise">
+						<Franchise />
+					</Route>
+					<Route path="/About">
+						<div>About</div>
+					</Route>
+					<Route path="/Contact">
+						<div>Contact</div>
+					</Route>
+				</Switch>
+			</ScrollToTop>
 			<Footer />
 		</Router>
 	);
